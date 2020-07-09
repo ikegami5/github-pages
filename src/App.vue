b88970cd59164eabaff62c976624414e6a26a163
ikegami5 <important2nd@gmail.com>
2020-07-09 14:40:52 +0900

    add links

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,23 @@
 <template>
     <div id="app">
-        <img alt="Vue logo" src="./assets/logo.png" />
-        <HelloWorld msg="I.T. の GitHub Pages" />
+        <Links msg="I.T. の GitHub Pages" />
     </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Links from "./components/Links.vue";
 
 export default {
-    name: "App",
+    name: 'App',
     components: {
-        HelloWorld
+        Links
     }
 };
 </script>
 
 <style>
 #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
+    font-family: "Helvetica Neue", Arial, "Hiragino Kaku Gothic ProN", "Hiragino Sans", Meiryo, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
---
+++ src/components/Links.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="links">
+        <h1>{{ msg }}</h1>
+        <h3>作ったもの</h3>
+        <ul>
+            <li>
+                <a href="https://mathq2nd.com/webapp/">MathQ - 数学・算数質問サイト</a>
+            </li>
+            <li>
+                <a href="https://blog.mathq2nd.com/">I.T. のエンジニアブログ</a>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Links',
+    props: {
+        msg: String
+    }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+    margin: 40px 0 0;
+}
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+li {
+    margin: 1rem;
+}
+a {
+    color: #42b983;
+}
+</style>
